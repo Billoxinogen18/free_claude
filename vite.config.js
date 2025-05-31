91c51b1635d2760a9ec7fb978875e9bbfbbb9784
Israel Bill <bisrael88@gmail.com>
2025-05-31 08:04:22 +0300

    Vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Import the 'path' module for robust path resolution
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Set the input to your index.html file within the public directory
+      input: path.resolve(__dirname, 'public/index.html')
+    }
+  }
 })
